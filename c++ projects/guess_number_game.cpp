32a7e3a5fd7c8a56e8741a125dd19f80a7876547
skcode1 <129771131+skcode1@users.noreply.github.com>
2025-05-05 17:44:36 +0000

    Update guess_number_game.cpp

--- c++ projects/guess_number_game.cpp
+++ c++ projects/guess_number_game.cpp
@@ -1,27 +1,34 @@
-#include<iostream>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 
-using namespace std;
-enum level {
-    easy=1  ,
-    medium,
-    hard
-};
-int main(){
-    cout << "NUMBER GUESSING GAME" << endl;
-   int levelinput;
-   cout << "press 1 for easy, press 2 for medium, press 3 for hard" << endl;
-       cout << "select level :" << endl;
-    cin >> levelinput;
-    switch (levelinput){
-case easy : cout << " you get 10 chances" << endl;
-break;
-case medium : cout << " you get 7 chances" << endl;
-break;
-case hard : cout << " you get 5 chances" << endl;
-default : cout << " invalid level " << endl;
-break;
-    }
- 
+int main() {
+    // Initialize random seed
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+    
+    // Generate a random number between 1 and 100
+    int secretNumber = std::rand() % 100 + 1;
+    int guess;
+    int attempts = 0;
+
+    std::cout << "Welcome to the Number Guessing Game!\n";
+    std::cout << "I have selected a number between 1 and 100.\n";
+
+    // Game loop
+    do {
+        std::cout << "Enter your guess: ";
+        std::cin >> guess;
+        attempts++;
+
+        if (guess > secretNumber) {
+            std::cout << "Too high! Try again.\n";
+        } else if (guess < secretNumber) {
+            std::cout << "Too low! Try again.\n";
+        } else {
+            std::cout << "Congratulations! You guessed the number in " << attempts << " attempts.\n";
+        }
+
+    } while (guess != secretNumber);
+
+    return 0;
 }
-
-  
